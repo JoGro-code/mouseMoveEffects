69e466da8cb5331b9ad11bc601f23c156f6c823a
JoGro-code <78618089+JoGro-code@users.noreply.github.com>
2023-12-07 08:02:51 +0100

    Add fluid

---
+++ 3/JS/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const canvas = document.getElementById("renderSurface");
+const myFluid = new Fluid(canvas);
+
+myFluid.activate();
+
+document.addEventListener("DOMContentLoaded", () => {
+  myFluid.activate();
+});
